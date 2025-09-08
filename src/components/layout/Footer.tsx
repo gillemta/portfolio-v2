f8151688d8575384a657c701bc0c728fe50819db
gillemta <tashan.gillem@gmail.com>
2025-09-08 00:11:15 -0400

    Fixed footer, added download button functionality and added hover states

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,17 +2,14 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export function Footer() {
     return (
-        <footer className="h-32 bg-primary text-secondary">
-            { }
+        <footer className="bg-primary text-secondary pt-8">
             <div className="container">
-                <div className="grid grid-cols-1 gap-6">
-                    <div className="flex justify-end space-x-4">
+                <div className="flex flex-col items-center justify-center min-h-32 gap-6">
+                    <div className="flex justify-end space-x-4 w-full">
                         <FaGithubSquare size={30} />
                         <FaLinkedin size={30} />
                     </div>
-
                     <div className="w-full border-t border-accent" />
-
                     <div className="text-center">
                         <p>© Copyright 2025 Tashan. All rights reserved.</p>
                     </div>
